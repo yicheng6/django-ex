b67ea5ccb3119f181ac48639a3477326277108f1
yicheng.liu <yicheng.liu@ximalaya.com>
2017-05-27 12:45:50 +0800

    fix

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ urls = (
 
 class index:
     def GET(self):
-    	# thread = threading.Thread(target=fetch.fetch,args=(web.input().id,))
-    	# thread.start()
-    	fetch.fetch(web.input().id)
+    	thread = threading.Thread(target=fetch.fetch,args=(web.input().id,))
+    	thread.start()
+    	# fetch.fetch(web.input().id)
         return "start fetch"
 
 if __name__ == "__main__":
--- fetch.py
+++ fetch.py
@@ -4,12 +4,15 @@ import json
 import time
 import os
 import helper
+import logging
 from random import choice
 
 import sys
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 host = os.getenv('MANAGE_HOST')
 sleepTimes = [0.7,0.1,0.2,0.3,0.4,0.5,0.6]
 headers = { 'User-Agent' : 'Baiduspider' }
@@ -18,10 +21,10 @@ def fetch(id=''):
 	key = long(id)
 	while True:
 		if not crawl(key):
-			print 'stop:%s'%key
+			logger.info('stop:%s'%key)
 			break
 		key = key - 1
-	print 'finish:%s'%key
+	logger.info('finish:%s'%key)
 
 def crawl(key=None):
 	try:
@@ -51,27 +54,27 @@ def crawl(key=None):
 			summary = result['summary']
 			price = result['price']
 			if average == '0.0':
-				print 'msg:%s average is 0.0'%id
+				logger.info('msg:%s average is 0.0'%id)
 			elif len(author) == 0:
-				print 'msg:%s author not exists'%id
+				logger.info('msg:%s author not exists'%id)
 			elif len(tags) == 0:
-				print 'msg:%s tags not exists'%id
+				logger.info('msg:%s tags not exists'%id)
 			elif isbn == None:
-				print 'msg:%s isbn13 not exists'%id
+				logger.info('msg:%s isbn13 not exists'%id)
 			else:
 				tagStrs = []
 				for tag in tags:
 					tagStrs.append(tag['name'])
-				print '%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n'%(id,title,','.join(author),isbn,publisher,pubdate,','.join(tagStrs),image,summary,price,average)
+				logger.info('%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n'%(id,title,','.join(author),isbn,publisher,pubdate,','.join(tagStrs),image,summary,price,average))
 				record_url = 'http://%s/fetch-api1/douban'%host
 				record_values = {'id':id,'title':title,'author':','.join(author),'isbn':isbn,'publisher':publisher,'pubdate':pubdate,'tags':','.join(tagStrs),'image':image,'summary':summary,'price':price,'average':average}
 				record_data = urllib.urlencode(record_values)
 				response_record = urllib.urlopen('%s?%s'%(record_url,record_data))
 		else:
-			print 'msg:%s\n'%(msg)
+			logger.info('msg:%s\n'%(msg))
 	except urllib2.URLError, e:
 	    if hasattr(e,"code"):
-	        print e.code
+	        logger.info(e.code)
 	    else:
 	    	raise e
 	time.sleep(choice(sleepTimes))
